ee0dc6e9ee3e5ae3d118207b1df184d940a2ed09
Timo Sztyler <timo@informatik.uni-mannheim.de>
2017-01-19 10:09:52 +0100

    Update

--- src/de/unima/sensor/features/example/UsageExample.java
+++ src/de/unima/sensor/features/example/UsageExample.java
@@ -14,7 +14,7 @@ import java.util.TreeMap;
  * Usage example how these libary has to be used to process acceleration data.
  *
  * @author Timo Sztyler
- * @version 30.11.2016
+ * @version 01.12.2016
  */
 public class UsageExample {
     public static void main(String[] args) throws Exception {
@@ -35,29 +35,29 @@ public class UsageExample {
             }
         }
 
-        //        File fileMag = new File("data/example/mag_walking_shin.csv");
-        //        try (BufferedReader br = new BufferedReader(new FileReader(fileMag))) {
-        //            String line;
-        //            while ((line = br.readLine()) != null) {
-        //                String[] fragments = line.split(",");
-        //                long     timestamp = Long.parseLong(fragments[1]);
-        //                float[]  values    = new float[]{Float.parseFloat(fragments[2]), Float.parseFloat(fragments[3]), Float.parseFloat(fragments[4])};
-        //
-        //                ff.addRecord(SensorType.MAGNETOMETER, timestamp, values, "walking", "shin", "mall", "shopping");
-        //            }
-        //        }
-        //
-        //        File fileGyr = new File("data/example/gyr_walking_shin.csv");
-        //        try (BufferedReader br = new BufferedReader(new FileReader(fileGyr))) {
-        //            String line;
-        //            while ((line = br.readLine()) != null) {
-        //                String[] fragments = line.split(",");
-        //                long     timestamp = Long.parseLong(fragments[1]);
-        //                float[]  values    = new float[]{Float.parseFloat(fragments[2]), Float.parseFloat(fragments[3]), Float.parseFloat(fragments[4])};
-        //
-        //                ff.addRecord(SensorType.GYROSCOPE, timestamp, values, "walking", "shin", "mall", "shopping");
-        //            }
-        //        }
+        File fileMag = new File("data/example/mag_walking_shin.csv");
+        try (BufferedReader br = new BufferedReader(new FileReader(fileMag))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fragments = line.split(",");
+                long     timestamp = Long.parseLong(fragments[1]);
+                float[]  values    = new float[]{Float.parseFloat(fragments[2]), Float.parseFloat(fragments[3]), Float.parseFloat(fragments[4])};
+
+                ff.addRecord(SensorType.MAGNETOMETER, timestamp, values, "walking", "shin", "mall", "shopping");
+            }
+        }
+
+        File fileGyr = new File("data/example/gyr_walking_shin.csv");
+        try (BufferedReader br = new BufferedReader(new FileReader(fileGyr))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fragments = line.split(",");
+                long     timestamp = Long.parseLong(fragments[1]);
+                float[]  values    = new float[]{Float.parseFloat(fragments[2]), Float.parseFloat(fragments[3]), Float.parseFloat(fragments[4])};
+
+                ff.addRecord(SensorType.GYROSCOPE, timestamp, values, "walking", "shin", "mall", "shopping");
+            }
+        }
 
 
         NavigableMap<Long, Window> result = new TreeMap<>();
